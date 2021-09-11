7fee2033ff91182e2e4a4c666a8647148f505676
Ivan Yushchuk <yuschuk.ivan@gmail.com>
2021-09-11 22:30:11 +0200

    Fixed tests

--- src/test/kotlin/fundamentals/section1/exercises/Ex_1_1_19KtTest.kt
+++ src/test/kotlin/fundamentals/section1/exercises/Ex_1_1_19KtTest.kt
@@ -7,21 +7,21 @@ class Ex_1_1_19KtTest{
 
     @Test
     fun testIterative() {
-        val actual = fibonacciIterative(30)
+        val actual = fibonacciIterative(10)
         val expected = 55
         assertEquals(expected, actual)
     }
 
     @Test
     fun testArray() {
-        val actual = fibonacciArray(30)
+        val actual = fibonacciArray(10)
         val expected = 55
         assertEquals(expected, actual)
     }
 
     @Test
     fun testRecursive() {
-        val actual = fibonacciRecursive(30)
+        val actual = fibonacciRecursive(10)
         val expected = 55
         assertEquals(expected, actual)
     }
